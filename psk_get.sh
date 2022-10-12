1539960fb0860b09b91f2a38ded9b3dd4a40f277
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-12 15:34:43 -0400

    added script to fetch 7d data from pskreporter

---
+++ psk_get.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CALLSIGN="KC1QBY"
+DATADIR="$(PWD)/psk_data"
+
+mkdir -p "$DATADIR"
+
+wget "https://pskreporter.info/cgi-bin/pskdata.pl?callsign=$CALLSIGN" -O "$DATADIR"/temp.zip
+unzip -d "$DATADIR" "$DATADIR"/temp.zip
+mv "$DATADIR"/psk_data.csv "$DATADIR"/$(date +%Y-%m-%d)_psk.csv
+rm "$DATADIR"/temp.zip
