776fa4528fcc0b58396f4e38b1fdfd1ac87d8fd0
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-14 13:26:27 -0400

    docker versioning

--- psk_get_docker.sh
+++ psk_get_docker.sh
@@ -20,7 +20,7 @@ sed -i  's/\([^,]\)"\([^,]\)/\1\2/g' "$DATADIR"/$(date +%Y-%m-%d)_psk.csv
 #APPEND TO DB
 docker exec -i prop-e2e-pipeline-postgres-1 psql -d $DB -U $USER --command="CREATE TEMP TABLE tmp_table ON COMMIT DROP AS SELECT * FROM pskreporter_raw; \
 COPY tmp_table \
-FROM '$DOCKERDATADIR/psk_data/$(date +%Y-%m-%d)_psk.csv' \
+FROM '$DOCKERDATADIR/$(date +%Y-%m-%d)_psk.csv' \
 WITH (FORMAT CSV, HEADER, DELIMITER ','); \
 INSERT INTO pskreporter_raw \
 SELECT * FROM tmp_table \
