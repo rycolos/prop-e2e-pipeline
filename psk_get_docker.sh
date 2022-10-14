2dbab58a487c7bb3186f8cff951790c8cbd4e82b
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-14 16:27:48 -0400

    docker sql updates

--- psk_get_docker.sh
+++ psk_get_docker.sh
@@ -9,15 +9,18 @@ USER="postgres"
 mkdir -p "$DATADIR"
 
 #GET FILE
+echo "Getting latest data..."
 wget "https://pskreporter.info/cgi-bin/pskdata.pl?callsign=$CALLSIGN" -O "$DATADIR"/temp.zip
 unzip -d "$DATADIR" "$DATADIR"/temp.zip
 mv "$DATADIR"/psk_data.csv "$DATADIR"/$(date +%Y-%m-%d)_psk.csv
 rm "$DATADIR"/temp.zip
 
 #CLEAN WITH SED (remove extra double quotes from antennainfo field)
+echo "Cleaning data..."
 sed -i  's/\([^,]\)"\([^,]\)/\1\2/g' "$DATADIR"/$(date +%Y-%m-%d)_psk.csv
 
 #APPEND TO DB
+echo "Appending to DB..."
 docker exec -i prop-e2e-pipeline-postgres-1 psql -d $DB -U $USER --command="CREATE TEMP TABLE tmp_table ON COMMIT DROP AS SELECT * FROM pskreporter_raw; \
 COPY tmp_table \
 FROM '$DOCKERDATADIR/$(date +%Y-%m-%d)_psk.csv' \
