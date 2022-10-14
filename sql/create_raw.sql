a3e6a20a4351a56a831d018d53fcf8e4fdb7b6a9
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-14 12:33:08 -0400

    restructure

---
+++ sql/create_raw.sql
@@ -0,0 +1,17 @@
+CREATE TABLE pskreporter_raw (
+    sNR TEXT NOT NULL,
+    mode TEXT,
+    MHz TEXT NOT NULL,
+    rxTime TEXT NOT NULL,
+    senderDXCC TEXT,
+    flowStartSeconds TEXT,
+    senderCallsign TEXT NOT NULL,
+    senderLocator TEXT NOT NULL,
+    receiverCallsign TEXT NOT NULL,
+    receiverLocator TEXT NOT NULL,
+    receiverAntennaInformation TEXT,
+    senderDXCCADIF TEXT,
+    submode TEXT,
+	CONSTRAINT comp_key PRIMARY KEY (rxTIME, senderCallsign, receiverCallsign),
+	CONSTRAINT check_kc1qby CHECK (receiverCallsign LIKE '%KC1QBY%' OR senderCallsign LIKE '%KC1QBY%')
+)
