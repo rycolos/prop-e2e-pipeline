1c6cca1cb91d063382f7fc72d54efbefe698d497
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-15 09:51:13 -0400

    new script to load all data

--- psk_load_all.sh
+++ psk_load_all.sh
@@ -6,9 +6,9 @@ USER="postgres"
 
 #APPEND TO RAW DB
 echo "Appending to DB..."
-docker exec -i prop-e2e-pipeline-postgres-1 psql -d $DB -U $USER --command="CREATE TEMP TABLE tmp_table ON COMMIT DROP AS SELECT * FROM pskreporter_raw; \
+docker exec -i prop-e2e-pipeline-postgres-1 cat $DOCKERDATADIR/*_psk.csv | psql -d $DB -U $USER --command="CREATE TEMP TABLE tmp_table ON COMMIT DROP AS SELECT * FROM pskreporter_raw; \
 COPY tmp_table \
-FROM '$DOCKERDATADIR/*_psk.csv' \
+FROM stdin \
 WITH (FORMAT CSV, HEADER, DELIMITER ','); \
 INSERT INTO pskreporter_raw \
 SELECT * FROM tmp_table \
