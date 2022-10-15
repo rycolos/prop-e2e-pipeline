2ea41a975183764d0df06f7f71e5e07e7015292f
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-15 06:50:45 -0400

    sql job refactor

--- sql/create_staged.sql
+++ sql/create_staged.sql
@@ -12,6 +12,6 @@ CREATE TABLE pskreporter_staged (
     receiverLocator TEXT,
     receiverLat REAL,
     receiverLon REAL,
-    CONSTRAINT staged_prim_key PRIMARY KEY (rxTime, senderCallsign, receiverCallsign),
+    CONSTRAINT staged_prim_key PRIMARY KEY (rxTime_utc, senderCallsign, receiverCallsign),
     CONSTRAINT staged_check_kc1qby CHECK (receiverCallsign LIKE '%KC1QBY%' OR senderCallsign LIKE '%KC1QBY%')
 )
