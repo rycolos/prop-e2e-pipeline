f8ec52461fea2cb64378dd76554d4121c7be0497
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-15 08:12:11 -0400

    update-staged distance addition

--- sql/create_staged.sql
+++ sql/create_staged.sql
@@ -12,6 +12,7 @@ CREATE TABLE pskreporter_staged (
     receiverLocator TEXT,
     receiverLat REAL,
     receiverLon REAL,
+    distance_mi REAL,
     CONSTRAINT staged_prim_key PRIMARY KEY (rxTime_utc, senderCallsign, receiverCallsign),
     CONSTRAINT staged_check_kc1qby CHECK (receiverCallsign LIKE '%KC1QBY%' OR senderCallsign LIKE '%KC1QBY%')
 )
