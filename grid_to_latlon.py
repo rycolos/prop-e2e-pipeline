e4ff4e2667433c7dca3db857d0ffd53f510c4f9f
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-15 05:11:22 -0400

    added try-except to grid>latlon script

--- grid_to_latlon.py
+++ grid_to_latlon.py
@@ -1,9 +1,12 @@
 import psycopg2
 
-def to_location(maiden):
-    maiden = maiden.strip().upper()
+def to_location(grid):
+    # convert maidenhead grid square to lat, lon
+    # from https://github.com/space-physics/maidenhead
+    
+    grid = grid.strip().upper()
 
-    N = len(maiden)
+    N = len(grid)
     if not 8 >= N >= 2 and N % 2 == 0:
         raise ValueError("Maidenhead locator requires 2-8 characters, even number of characters")
 
@@ -11,25 +14,23 @@ def to_location(maiden):
     lon = -180.0
     lat = -90.0
     # first pair
-    lon += (ord(maiden[0]) - Oa) * 20
-    lat += (ord(maiden[1]) - Oa) * 10
+    lon += (ord(grid[0]) - Oa) * 20
+    lat += (ord(grid[1]) - Oa) * 10
     # second pair
     if N >= 4:
-        lon += int(maiden[2]) * 2
-        lat += int(maiden[3]) * 1
+        lon += int(grid[2]) * 2
+        lat += int(grid[3]) * 1
+     # third pair
     if N >= 6:
-        lon += (ord(maiden[4]) - Oa) * 5.0 / 60
-        lat += (ord(maiden[5]) - Oa) * 2.5 / 60
+        lon += (ord(grid[4]) - Oa) * 5.0 / 60
+        lat += (ord(grid[5]) - Oa) * 2.5 / 60
+     # fourth pair
     if N >= 8:
-        lon += int(maiden[6]) * 5.0 / 600
-        lat += int(maiden[7]) * 2.5 / 600
+        lon += int(grid[6]) * 5.0 / 600
+        lat += int(grid[7]) * 2.5 / 600
     return lat, lon
 
-conn = psycopg2.connect(database="prop-e2e",
-                        host="192.168.1.91",
-                        user="postgres",
-                        password="postgres",
-                        port="5432")
+conn = psycopg2.connect(database="prop-e2e", host="192.168.1.91", user="postgres", password="postgres", port="5432")
 
 cur = conn.cursor()
 cur.execute("SELECT id, senderLocator, senderLat, senderLon, receiverLocator, receiverLat, receiverLon FROM pskreporter_staged FOR UPDATE")
@@ -39,8 +40,14 @@ for row in result:
     if row[2] is None or row[3] is None:
         sender = row[1]
         sender = sender[0:8]
-        senderLat, senderLon = to_location(sender)
+        
+        try:
+            senderLat, senderLon = to_location(sender)
+        except Exception:
+            continue
+        
         print(sender, senderLat, senderLon)
+        
         cur.execute(f"UPDATE pskreporter_staged SET senderlat = {senderLat} WHERE id = {row[0]}")
         cur.execute(f"UPDATE pskreporter_staged SET senderlon = {senderLon} WHERE id = {row[0]}")
         conn.commit()
@@ -48,8 +55,14 @@ for row in result:
     if row[5] is None or row[6] is None:
         receiver = row[4]
         receiver = receiver[0:8]
-        receiverLat, receiverLon = to_location(receiver)
+        
+        try:
+            receiverLat, receiverLon = to_location(receiver)
+        except Exception:
+            continue
+
         print(receiver, receiverLat, receiverLon) 
+        
         cur.execute(f"UPDATE pskreporter_staged SET receiverlat = {receiverLat} WHERE id = {row[0]}")
         cur.execute(f"UPDATE pskreporter_staged SET receiverlon = {receiverLon} WHERE id = {row[0]}")
         conn.commit()
