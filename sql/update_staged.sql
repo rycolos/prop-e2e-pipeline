6e2d7a92dd3e49b499f8a59e6d9ceff90e270af5
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-15 11:55:19 -0400

    refactor, reorgs

--- sql/update_staged.sql
+++ sql/update_staged.sql
@@ -10,7 +10,3 @@ SELECT
     receiverLocator
 FROM pskreporter_raw
 ON CONFLICT DO NOTHING;
-
-UPDATE pskreporter_staged
-SET distance_mi = CAST(SQRT(POW(69.1 * (senderlat -  receiverlat), 2) + POW(69.1 * (receiverlon - senderlon) * COS(senderlat / 57.3), 2)) AS REAL)
-WHERE distance_mi IS NULL;
