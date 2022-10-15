d87bafb3ba091da550c1239379f0f91b2ef236b3
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-15 05:21:54 -0400

    added received and received_by views

---
+++ sql/create_view_received.sql
@@ -0,0 +1,5 @@
+CREATE VIEW received AS
+    SELECT
+		*
+	FROM pskreporter_staged
+	WHERE receivercallsign = 'KC1QBY';
---
+++ sql/create_view_received_by.sql
@@ -0,0 +1,5 @@
+CREATE VIEW received_by AS
+    SELECT
+		*
+	FROM pskreporter_staged
+	WHERE sendercallsign = 'KC1QBY';
