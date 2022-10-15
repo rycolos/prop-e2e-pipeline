1ec758cc1c077dfe60870e08dbce213a7e7aafd9
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-15 11:00:06 -0400

    new script to load all data

---
+++ psk_load_all.py
@@ -0,0 +1,40 @@
+import os, psycopg2, re
+
+CALLSIGN="KC1QBY"
+DATADIR="$(pwd)/postgres_data/psk_data"
+DOCKERDATADIR="/var/lib/postgresql/data/psk_data"
+DB="prop-e2e"
+USER="postgres"
+
+unpruned_f = os.listdir("/Users/ryanlaliberty/Desktop/Coding - Working On/prop-e2e-pipeline/")
+pruned_f = []
+
+#remove from list if not ending in .sh
+for index, item in enumerate(unpruned_f):
+    #print(f"{index} - {item}")
+    if re.match('^.*\.csv$', item):
+        pruned_f.append(item)
+
+print(pruned_f)
+
+conn = psycopg2.connect(database="prop-e2e", host="192.168.1.91", user="postgres", password="postgres", port="5432")
+cur = conn.cursor()
+
+for item in pruned_f:
+    cmd = f'''
+    CREATE TEMP TABLE tmp_table ON COMMIT DROP AS SELECT * FROM pskreporter_raw; \
+    COPY tmp_table \
+    FROM '{DOCKERDATADIR}/{item}' \
+    WITH (FORMAT CSV, HEADER, DELIMITER ','); \
+    INSERT INTO pskreporter_raw \
+    SELECT * FROM tmp_table \
+    ON CONFLICT DO NOTHING;"
+    '''
+
+    cursor.execute(cmd)
+
+    for i in cursor.fetchall():
+        print(i)
+  
+conn.commit()
+conn.close()
--- psk_load_all.sh
+++ psk_load_all.sh
@@ -2,14 +2,15 @@
 
 DATADIR="$(pwd)/postgres_data/psk_data"
 DOCKERDATADIR="/var/lib/postgresql/data/psk_data"
+FILES=($(pwd)/postgres_data/psk_data/*_psk.csv)
 DB="prop-e2e"
 USER="postgres"
 
 #APPEND TO RAW DB
 echo "Appending to DB..."
-docker exec -i prop-e2e-pipeline-postgres-1 cat $DOCKERDATADIR/*_psk.csv | psql -d $DB -U $USER --command="CREATE TEMP TABLE tmp_table ON COMMIT DROP AS SELECT * FROM pskreporter_raw; \
+docker exec -i prop-e2e-pipeline-postgres-1 cat $DATADIR/*_psk.csv | psql -d $DB -U $USER --command="CREATE TEMP TABLE tmp_table ON COMMIT DROP AS SELECT * FROM pskreporter_raw; \
 COPY tmp_table \
-FROM stdin \
+FROM '$DOCKERDATADIR/' \
 WITH (FORMAT CSV, HEADER, DELIMITER ','); \
 INSERT INTO pskreporter_raw \
 SELECT * FROM tmp_table \
