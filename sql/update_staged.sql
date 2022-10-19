24a06cc772383a255770380f922c59861bb74519
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-19 09:59:58 -0400

    added drop-tables to makefile, testing auto-date insert column in raw

--- sql/update_staged.sql
+++ sql/update_staged.sql
@@ -1,6 +1,5 @@
-INSERT INTO pskreporter_staged (id, sNR, commMode, frequency, rxTime_utc, senderCallsign, senderLocator, receiverCallsign, receiverLocator)
+INSERT INTO pskreporter_staged (sNR, commMode, frequency, rxTime_utc, senderCallsign, senderLocator, receiverCallsign, receiverLocator)
 SELECT
-    id,
     CAST(sNR AS INT),
     mode,
     CAST(MHz AS DOUBLE PRECISION),
