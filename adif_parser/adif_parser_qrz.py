3215663cbad3737c42cde6be5bfbf73c76ba53b4
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-19 06:16:18 -0400

    updating adif parser

--- adif_parser/adif_parser_qrz.py
+++ adif_parser/adif_parser_qrz.py
@@ -2,6 +2,7 @@ import os, pandas as pd, re
 
 #modified from https://github.com/bjorgan/adif_parser
 #designed to work only with QRZ.com exports
+#currently, all columns must have a value to parse correctly
 
 app_qrzlog_logid_COL = 'app_qrzlog_logid'
 app_qrzlog_qsldate_COL = 'app_qrzlog_qsldate'
@@ -77,7 +78,6 @@ def extract_adif_column(adif_file, col):
         return matches
     else:
         return None
-
 def parse_adif(filename):
    #comment out unwanted rows
    #need a better way to handle rows that don't exist
@@ -104,36 +104,36 @@ def parse_adif(filename):
                'app_qrzlog_logid': extract_adif_column(adif_file, app_qrzlog_logid_COL),
                #'app_qrzlog_qsldate': extract_adif_column(adif_file, app_qrzlog_qsldate_COL),
                #'app_qrzlog_status': extract_adif_column(adif_file, app_qrzlog_status_COL),
-               'band': extract_adif_column(adif_file, band_COL),
-               'band_rx': extract_adif_column(adif_file, band_rx_COL),
+               #'band': extract_adif_column(adif_file, band_COL),
+               #'band_rx': extract_adif_column(adif_file, band_rx_COL),
                'call': extract_adif_column(adif_file, call_COL),
                #'cnty': extract_adif_column(adif_file, cnty_COL),
-               'comment': extract_adif_column(adif_file, comment_COL),
-               'cont': extract_adif_column(adif_file, cont_COL),
+               #'comment': extract_adif_column(adif_file, comment_COL),
+               #'cont': extract_adif_column(adif_file, cont_COL),
                'country': extract_adif_column(adif_file, country_COL),
-               'cqz': extract_adif_column(adif_file, cqz_COL),
-               'distance': extract_adif_column(adif_file, distance_COL),
-               'dxcc': extract_adif_column(adif_file, dxcc_COL),
+               #'cqz': extract_adif_column(adif_file, cqz_COL),
+               #'distance': extract_adif_column(adif_file, distance_COL),
+               #'dxcc': extract_adif_column(adif_file, dxcc_COL),
                #'email': extract_adif_column(adif_file, email_COL),
                #'eqsl_qsl_rcvd': extract_adif_column(adif_file, eqsl_qsl_rcvd_COL),
                #'eqsl_qsl_sent': extract_adif_column(adif_file, eqsl_qsl_sent_COL),
                'freq': extract_adif_column(adif_file, freq_COL),
-               'freq_rx': extract_adif_column(adif_file, freq_rx_COL),
+               #'freq_rx': extract_adif_column(adif_file, freq_rx_COL),
                'gridsquare': extract_adif_column(adif_file, gridsquare_COL),
-               'ituz': extract_adif_column(adif_file, ituz_COL),
-               'lat': extract_adif_column(adif_file, lat_COL),
-               'lon': extract_adif_column(adif_file, lon_COL),
+               #'ituz': extract_adif_column(adif_file, ituz_COL),
+               #'lat': extract_adif_column(adif_file, lat_COL),
+               #'lon': extract_adif_column(adif_file, lon_COL),
                #'lotw_qsl_rcvd': extract_adif_column(adif_file, lotw_qsl_rcvd_COL),
                #'lotw_qsl_sent': extract_adif_column(adif_file, lotw_qsl_sent_COL),
                'mode': extract_adif_column(adif_file, mode_COL),
-               'my_city': extract_adif_column(adif_file, my_city_COL),
-               'my_cnty': extract_adif_column(adif_file, my_cnty_COL),
+               #'my_city': extract_adif_column(adif_file, my_city_COL),
+               #'my_cnty': extract_adif_column(adif_file, my_cnty_COL),
                'my_country': extract_adif_column(adif_file, my_country_COL),
-               'my_cq_zone': extract_adif_column(adif_file, my_cq_zone_COL),
+               #'my_cq_zone': extract_adif_column(adif_file, my_cq_zone_COL),
                'my_gridsquare': extract_adif_column(adif_file, my_gridsquare_COL),
-               'my_itu_zone': extract_adif_column(adif_file, my_itu_zone_COL),
-               'my_lat': extract_adif_column(adif_file, my_lat_COL),
-               'my_lon': extract_adif_column(adif_file, my_lon_COL),
+               #'my_itu_zone': extract_adif_column(adif_file, my_itu_zone_COL),
+               #'my_lat': extract_adif_column(adif_file, my_lat_COL),
+               #'my_lon': extract_adif_column(adif_file, my_lon_COL),
                #'my_name': extract_adif_column(adif_file, my_name_COL),
                #'name': extract_adif_column(adif_file, name_COL),
                'qrzcom_qso_upload_date': extract_adif_column(adif_file, qrzcom_qso_upload_date_COL),
@@ -141,14 +141,14 @@ def parse_adif(filename):
                #'qsl_rcvd': extract_adif_column(adif_file, qsl_rcvd_COL),
                #'qsl_sent': extract_adif_column(adif_file, qsl_sent_COL),
                'qso_date': extract_adif_column(adif_file, qso_date_COL),
-               'qso_date_off': extract_adif_column(adif_file, qso_date_off_COL),
-               'qth': extract_adif_column(adif_file, qth_COL),
+               #'qso_date_off': extract_adif_column(adif_file, qso_date_off_COL),
+               #'qth': extract_adif_column(adif_file, qth_COL),
                'rst_rcvd': extract_adif_column(adif_file, rst_rcvd_COL),
                'rst_sent': extract_adif_column(adif_file, rst_sent_COL),
                #'state': extract_adif_column(adif_file, state_COL),
                'station_callsign': extract_adif_column(adif_file, station_callsign_COL),
                'time_off': extract_adif_column(adif_file, time_off_COL),
-               'time_on': extract_adif_column(adif_file, time_on_COL),
+               #'time_on': extract_adif_column(adif_file, time_on_COL),
                'tx_pwr': extract_adif_column(adif_file, tx_pwr_COL)
                })
     except:
