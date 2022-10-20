e0ba9f8f6e9fc126dbc1a630d2f889afa0b91724
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-20 15:43:55 -0400

    added logbook_load script for testing

--- sql/load_logbook.sql
+++ sql/load_logbook.sql
@@ -1,56 +1,60 @@
-CREATE TEMP TABLE tmp_log_table ON COMMIT DROP AS SELECT 
-	app_qrzlog_logid, 
-	call, 
-	frequency, 
-	country, 
-	gridsquare, 
-	mode, 
-	my_country, 
-	my_gridsquare, 
-	qrzcom_qso_upload_date, 
-	qso_date, 
-	rst_rcvd, 
-	rst_sent, 
-	station_callsign, 
-	time_off, 
-	tx_pwr 
-FROM logbook_raw; 
-COPY tmp_log_table 
+COPY logbook_raw
 FROM '/home/kepler/prop-e2e-pipeline/adif_parser/adi-test.csv' 
 WITH (FORMAT CSV, HEADER, DELIMITER ','); 
 
-INSERT INTO logbook_raw (
-	app_qrzlog_logid, 
-	call, 
-	frequency, 
-	country, 
-	gridsquare, 
-	mode, 
-	my_country, 
-	my_gridsquare, 
-	qrzcom_qso_upload_date, 
-	qso_date, 
-	rst_rcvd, 
-	rst_sent, 
-	station_callsign, 
-	time_off, 
-	tx_pwr 
-)
-SELECT
-	app_qrzlog_logid, 
-	call, 
-	frequency, 
-	country, 
-	gridsquare, 
-	mode, 
-	my_country, 
-	my_gridsquare, 
-	qrzcom_qso_upload_date, 
-	qso_date, 
-	rst_rcvd, 
-	rst_sent, 
-	station_callsign, 
-	time_off, 
-	tx_pwr 
-FROM tmp_log_table
-ON CONFLICT DO NOTHING;
+-- CREATE TEMP TABLE tmp_log_table ON COMMIT DROP AS SELECT 
+-- 	app_qrzlog_logid, 
+-- 	call, 
+-- 	frequency, 
+-- 	country, 
+-- 	gridsquare, 
+-- 	mode, 
+-- 	my_country, 
+-- 	my_gridsquare, 
+-- 	qrzcom_qso_upload_date, 
+-- 	qso_date, 
+-- 	rst_rcvd, 
+-- 	rst_sent, 
+-- 	station_callsign, 
+-- 	time_off, 
+-- 	tx_pwr 
+-- FROM logbook_raw; 
+-- COPY tmp_log_table 
+-- FROM '/home/kepler/prop-e2e-pipeline/adif_parser/adi-test.csv' 
+-- WITH (FORMAT CSV, HEADER, DELIMITER ','); 
+
+-- INSERT INTO logbook_raw (
+-- 	app_qrzlog_logid, 
+-- 	call, 
+-- 	frequency, 
+-- 	country, 
+-- 	gridsquare, 
+-- 	mode, 
+-- 	my_country, 
+-- 	my_gridsquare, 
+-- 	qrzcom_qso_upload_date, 
+-- 	qso_date, 
+-- 	rst_rcvd, 
+-- 	rst_sent, 
+-- 	station_callsign, 
+-- 	time_off, 
+-- 	tx_pwr 
+-- )
+-- SELECT
+-- 	app_qrzlog_logid, 
+-- 	call, 
+-- 	frequency, 
+-- 	country, 
+-- 	gridsquare, 
+-- 	mode, 
+-- 	my_country, 
+-- 	my_gridsquare, 
+-- 	qrzcom_qso_upload_date, 
+-- 	qso_date, 
+-- 	rst_rcvd, 
+-- 	rst_sent, 
+-- 	station_callsign, 
+-- 	time_off, 
+-- 	tx_pwr 
+-- FROM tmp_log_table
+-- ON CONFLICT DO NOTHING;
