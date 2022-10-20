25f85aabc993006102ff58ca127bde09a9dd4e85
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-20 08:03:50 -0400

    added drop-tables to makefile, testing auto-date insert column in raw

--- psk_get_docker.sh
+++ psk_get_docker.sh
@@ -22,10 +22,10 @@ sed -i  's/\([^,]\)"\([^,]\)/\1\2/g' "$DATADIR"/$(date +%Y-%m-%d)_psk.csv
 
 #APPEND TO RAW DB
 echo "Appending to DB..."
-docker exec -i prop-e2e-pipeline-postgres-1 psql -d $DB -U $USER --command="CREATE TEMP TABLE tmp_table ON COMMIT DROP AS SELECT * FROM pskreporter_raw; \
+docker exec -i prop-e2e-pipeline-postgres-1 psql -d $DB -U $USER --command="CREATE TEMP TABLE tmp_table ON COMMIT DROP AS SELECT sNR, mode, mhz, rxTime, senderdxcc, flowstartseconds, senderCallsign, senderLocator, receiverCallsign, receiverLocator, receiverAntennaInformation, senderDXCCADIF, submode FROM pskreporter_raw; \
 COPY tmp_table \
 FROM '$DOCKERDATADIR/$(date +%Y-%m-%d)_psk.csv' \
 WITH (FORMAT CSV, HEADER, DELIMITER ','); \
-INSERT INTO pskreporter_raw \
-SELECT * FROM tmp_table \
+INSERT INTO pskreporter_raw (sNR, mode, mhz, rxTime, senderdxcc, flowstartseconds, senderCallsign, senderLocator, receiverCallsign, receiverLocator, receiverAntennaInformation, senderDXCCADIF, submode) \
+SELECT sNR, mode, mhz, rxTime, senderdxcc, flowstartseconds, senderCallsign, senderLocator, receiverCallsign, receiverLocator, receiverAntennaInformation, senderDXCCADIF, submode FROM tmp_table \
 ON CONFLICT DO NOTHING;"
