7660efea044b9ab8ffffbe599b27da40435c79bc
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-20 17:18:48 -0400

    added update_logbook sql

--- sql/update_staged_logb.sql
+++ sql/update_staged_logb.sql
@@ -1,17 +1,17 @@
 INSERT INTO logbook_staged (qso_date, time_off, frequency, comm_mode, receiver_callsign, receiver_locator, sender_callsign, sender_locator, rst_rcvd, rst_sent, tx_pwr, app_qrzlog_logid, qrzcom_qso_upload_date)
 SELECT
-    qso_date,
+    CAST(qso_date AS DATE),
     time_off,
-    frequency,
+    CAST(frequency AS DOUBLE PRECISION),
     mode,
     call,
     gridsquare,
     station_callsign,
     my_gridsquare,
-    rst_rcvd,
-    rst_sent,
-    tx_pwr,
-    app_qrzlog_logid,
-    qrzcom_qso_upload_date
+    CAST(rst_rcvd AS INTEGER),
+    CAST(rst_sent AS INTEGER),
+    CAST(tx_pwr AS INTEGER),
+    CAST(app_qrzlog_logid AS BIGINT),
+    CAST(qrzcom_qso_upload_date AS DATE)
 FROM logbook_raw
 ON CONFLICT DO NOTHING;
